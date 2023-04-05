5e53da1b635dd5cf7a5386c615be6dfd16b3c62f
Amanda Pond <amandalynnpond@gmail.com>
2023-04-05 09:46:31 -0400

    Added reservation status to table and display on front-end

--- front-end/src/reservations/ReservationCard.js
+++ front-end/src/reservations/ReservationCard.js
@@ -8,9 +8,11 @@ function ReservationCard({reservation}){
         <article>
             <h4>{reservation.first_name} {reservation.last_name}</h4>
             {reservation.people} guests | {reservation.reservation_time}
+            <div data-reservation-id-status={reservation.reservation_id}>Status: {reservation.status}</div>
             <a href={`/reservations/${reservation_id}/seat`}>
                 <button type="button" className="btn btn-secondary">Seat</button>
             </a>
+
         </article>
     )
 }
